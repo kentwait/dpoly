7921e85dfd5aa40fd5537efabb80f2bb3cb988ff
kent <kentkawashima@gmail.com>
2018-11-08 14:34:32 +0900

    Moves functions from match.py to blast.py

---
+++ pipeline/blast.py
@@ -0,0 +1,131 @@
+import subprocess as proc
+import pandas as pd
+import numpy as np
+
+def create_blastdb(fasta_path, title, out_path,
+                   dbtype='nucl'):
+    """
+
+    Parameters
+    ----------
+    fasta_path : str
+    title : str
+    out_path : str
+    dbtype: str, optional
+
+    Returns
+    -------
+    subprocess.CompletedProcess
+    """
+    blast_cmd = 'makeblastdb -in {fasta_path} ' \
+                '-input_type fasta -dbtype {dbtype} -title {title} ' \
+                '-out {out_path}'
+    cmd = blast_cmd.format(fasta_path=fasta_path,
+                           dbtype=dbtype,
+                           title=title,
+                           out_path=out_path)
+    return proc.run(cmd, shell=True, stdout=proc.PIPE)
+
+def call_blast(task, query_path, db_path, out_path,
+               outfmt='6 std qlen slen qcovs sstrand',
+               max_seq=1,
+               threads=6):
+    """
+
+    Parameters
+    ----------
+    task : str
+    query_path : str
+    db_path : str
+    out_path : str
+    outfmt : str, optional
+    max_seq : int, optional
+    threads : int, optional
+
+    Returns
+    -------
+    subprocess.CompletedProcess
+    """
+    blast_cmd = 'blastn -task {task} -query {query} ' \
+                '-db {db} -out {out} -outfmt {outfmt} ' \
+                '-max_target_seqs {max_seq} -num_threads {threads}'
+    cmd = blast_cmd.format(task=task,
+                           query=query_path,
+                           db=db_path,
+                           out=out_path,
+                           outfmt='"'+outfmt+'"',
+                           max_seq=max_seq,
+                           threads=threads)
+    return proc.run(cmd, shell=True, stdout=proc.PIPE)
+
+def blast_to_df(path,
+                filter_by_eval=True,
+                eval_threshold=1e-10,
+                sep='\t',
+                col_labels=('qaccver', 'saccver', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore', 'qlen', 'slen', 'qcovs', 'sstrand')):
+    """Converts a tab-delimited blastn result to a pandas DataFrame.
+
+    Parameters
+    ----------
+    path : str
+    filter_by_eval : bool, optional
+    eval_threshold : float, optional
+    sep : str, optional
+    col_labels : iterable of str, optional
+
+    Returns
+    -------
+    pandas.DataFrame
+    """
+    df = pd.DataFrame.from_csv(path, sep=sep, header=None, index_col=None)
+    df.columns = col_labels
+    if filter_by_eval:
+        return df[df['evalue'] < eval_threshold]
+    return df
+
+def summarize(group):
+    """Reduces a group of entries into a single row.
+
+    Parameters
+    ----------
+    group : pandas.DataFrame
+
+    Returns
+    -------
+    pandas.Series
+    """
+    return pd.Series({
+        'qaccver': np.max(group['qaccver']),
+        'saccver': np.max(group['saccver']),
+        'pident': np.sum(group['pident'] * \
+                  (group['length'] / group['length'].sum())),
+        'length': group['length'].sum(),
+        'mismatch': group['mismatch'].sum(),
+        'gapopen': group['gapopen'].sum(),
+        'bitscore': group['bitscore'].sum(),
+        'qlen': np.max(group['qlen']),
+        'slen': np.max(group['slen']),
+    })
+
+def group_blastdf(df,
+                  summarize=True,
+                  groupby=('qaccver', 'saccver'),
+                  summary_col_labels=('qaccver', 'saccver', 'pident',
+                                      'length', 'mismatch', 'gapopen',
+                                      'bitscore', 'qlen', 'slen')):
+    """Groups blastn output based on the query and subject ID's.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+    groupby : iterable of str
+    summary_col_labels : iterable of str
+    
+    Returns
+    -------
+    pandas.DataFrame
+    """
+    grp_df = df.groupby(groupby)[summary_col_labels]
+    if summarize:
+        return grp_df.apply(summarize)[summary_col_labels[2:]]
+    return grp_df
--- pipeline/match.py
+++ pipeline/match.py
@@ -1,7 +1,3 @@
-import subprocess as proc
-from Bio import SeqIO
-import pandas as pd
-
 def match_by_fbtr(fbtr_list1, fbtr_list2):
     """Match FlyBase transcript ID's between 2 lists.
 
@@ -39,59 +35,3 @@ def reciprocal_match_by_fbtr(fbtr_list1, fbtr_list2):
     reverse_match = set(match_by_fbtr(fbtr_list2, fbtr_list1))
     return forward_match.intersection(reverse_match)
 
-def call_blast(task, query_path, db_path, out_path,
-               outfmt='6 std qlen slen qcovs sstrand',
-               max_seq=1,
-               threads=6):
-    """
-
-    Parameters
-    ----------
-    task : str
-    query_path : str
-    db_path : str
-    out_path : str
-    outfmt : str
-    max_seq : int
-    threads : int
-
-    Returns
-    -------
-    int
-    """
-    blast_cmd = 'blastn -task {task} -query {query} ' \
-                '-db {db} -out {out} -outfmt {outfmt} ' \
-                '-max_target_seqs {max_seq} -num_threads {threads}'
-    cmd = blast_cmd.format(task=task,
-                           query=query_path,
-                           db=db_path,
-                           out=out_path,
-                           outfmt='"'+outfmt+'"',
-                           max_seq=max_seq,
-                           threads=threads)
-    return proc.run(cmd, shell=True, stdout=proc.PIPE)
-
-def blast_to_df(path,
-                filter_by_eval=True,
-                eval_threshold=1e-10,
-                sep='\t',
-                col_labels=['qaccver', 'saccver', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore', 'qlen', 'slen', 'qcovs', 'sstrand']):
-    """Converts a tab-delimited blastn result to a pandas DataFrame.
-
-    Parameters
-    ----------
-    path : str
-    filter_by_eval : bool
-    eval_threshold : float
-    sep : str
-    col_labels : list of str
-
-    Returns
-    -------
-    pandas.DataFrame
-    """
-    df = pd.DataFrame.from_csv(path, sep=sep, header=None, index_col=None)
-    df.columns = col_labels
-    if filter_by_eval:
-        return df[df['evalue'] < eval_threshold]
-    return df
