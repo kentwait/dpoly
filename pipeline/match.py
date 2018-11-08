d254502e51a9d3709eeab7b210c101399e91d88d
kent <kentkawashima@gmail.com>
2018-11-08 13:05:25 +0900

    Adds blast_to_df

--- pipeline/match.py
+++ pipeline/match.py
@@ -1,5 +1,6 @@
-from Bio import SeqIO
 import subprocess as proc
+from Bio import SeqIO
+import pandas as pd
 
 def match_by_fbtr(fbtr_list1, fbtr_list2):
     """Match FlyBase transcript ID's between 2 lists.
@@ -69,3 +70,28 @@ def call_blast(task, query_path, db_path, out_path,
                            max_seq=max_seq,
                            threads=threads)
     return proc.run(cmd, shell=True, stdout=proc.PIPE)
+
+def blast_to_df(path,
+                filter_by_eval=True,
+                eval_threshold=1e-10,
+                sep='\t',
+                col_labels=['qaccver', 'saccver', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore', 'qlen', 'slen', 'qcovs', 'sstrand']):
+    """Converts a tab-delimited blastn result to a pandas DataFrame.
+
+    Parameters
+    ----------
+    path : str
+    filter_by_eval : bool
+    eval_threshold : float
+    sep : str
+    col_labels : list of str
+
+    Returns
+    -------
+    pandas.DataFrame
+    """
+    df = pd.DataFrame.from_csv(path, sep=sep, header=None, index_col=None)
+    df.columns = col_labels
+    if filter_by_eval:
+        return df[df['evalue'] < eval_threshold]
+    return df
